edd07fc14b03d8cdce6d853522941ce70e65d42c
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-12-24 09:39:38 +0200

    Commenting

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
 
     if (argc == 1) {
         // Interactive Mode
+        // Read input from user directly
 
         // Line characters store
         char* buffer;
@@ -19,20 +20,35 @@ int main(int argc, char* argv[]) {
         while ((buffer = readline("> ")) != nullptr) {
             if (strlen(buffer) == 0) continue; // Ignore empty lines
             line = string(buffer);
-            add_history(buffer); // add last read line to prompt history
-            free(buffer); // free buffer because readline always allocates a new buffer
-            cout << line << endl; // just print it
+            // add last read line to prompt history
+            add_history(buffer);
+            // free buffer because readline always allocates a new buffer
+            free(buffer);
+            // just print it
+            cout << line << endl;
         }
     } else if (argc == 3 && (strcmp(argv[1], "--file") == 0 || strcmp(argv[1], "-f") == 0)) {
+        // File Mode
         // Read input from file
+
+        // Open requested file for reading
         ifstream input_file {argv[2]};
+        // Seek to fil end
         input_file.seekg(0, std::ios::end);
+        // Read total file size
         u64 file_size = input_file.tellg();
+        // Allocate enough space to hold file contents
         char* input = new char[file_size];
+        // Seek back to beginning of file to start contents copy
         input_file.seekg(0, std::ios::beg);
+        // Read all characters
         input_file.read(input, file_size);
         printf("%s", input);
+
+        // Free input buffer
+        delete[] input;
     } else {
+        // Print help on how to use
         cerr << "Invalid command-line arguments\n" ;
         cerr << "Usage:\n" ;
         cerr << "   ./main\n" ;
