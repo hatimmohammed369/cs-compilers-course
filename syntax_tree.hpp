40c24d9c7f8ccea3405be26244a1256cb6f6a7f7
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-12-25 19:23:52 +0200

    Syntax tree base class

---
+++ syntax_tree.hpp
@@ -0,0 +1,16 @@
+#ifndef SYNTAX_TREE_H_INCLUDED
+#define SYNTAX_TREE_H_INCLUDED
+
+#include "common.h"
+
+class TreeBase {
+public:
+    ~TreeBase() {}
+    virtual std::string to_string() const noexcept = 0;
+};
+
+inline std::ostream& operator<<(std::ostream& os, const TreeBase& tree_node) {
+    return os << tree_node.to_string() ;
+}
+
+#endif
