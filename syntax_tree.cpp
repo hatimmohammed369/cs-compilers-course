2af28ea22009183d75baa53130f0580a16887cfa
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-12-30 07:27:14 +0200

    Logical expressions tree nodes

--- syntax_tree.cpp
+++ syntax_tree.cpp
@@ -1,4 +1,5 @@
 #include "syntax_tree.hpp"
+#include <sstream>
 
 Literal::Literal(Object* val): value_object{val} {}
 
@@ -8,4 +9,17 @@ std::string Literal::to_string() const noexcept {
 
 Object* Literal::accept(Visitor* visitor) {
     return visitor->visit_literal(this);
+}
+
+std::string Logical::to_string() const noexcept  {
+    std::ostringstream oss;
+    if (bang) oss << bang->value ;
+    oss << left->to_string() ;
+    oss << op.value ;
+    oss << right->to_string() ;
+    return oss.str() ;
+}
+
+Object* Logical::accept(Visitor* visitor)  {
+    return visitor->visit_logical(this);
 }
