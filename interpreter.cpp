6bd0dd943f90176af4f93fedd46f942547694c35
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-12-30 07:47:10 +0200

    Visiting Logical expressions

--- interpreter.cpp
+++ interpreter.cpp
@@ -7,3 +7,13 @@ Object* Interpreter::interpret(TreeBase* tree) {
 Object* Interpreter::visit_literal(Literal* literal) {
     return literal->value_object;
 }
+
+Object* Interpreter::visit_logical(Logical* logical) {
+    ObjectBoolean* left =
+         reinterpret_cast<ObjectBoolean*>(logical->left->accept(this));
+    ObjectBoolean* right =
+         reinterpret_cast<ObjectBoolean*>(logical->right->accept(this));
+    bool value = (logical->bang ? !left->get() : left->get());
+    return (value ? ObjectBoolean::get_true_object() : ObjectBoolean::get_false_object());
+}
+
