774ef732ec517637a677bacdcd9898f48636515e
Hatim Muhammed <hatimmohammed369@gmail.com>
2025-02-05 17:22:44 +0200

    Fixing method `parse_logical_and`

--- parser.cpp
+++ parser.cpp
@@ -205,29 +205,26 @@ ParseResult Parser::parse_logical_or() {
 
 ParseResult Parser::parse_logical_and() {
     ParseResult result = parse_bitwise_xor();
-    if (!result.error.empty() || !result.parsed_hunk)
-        return result;
     while (
-        result.error.empty() &&
+        result.is_ok() &&
         current.ttype == TokenType::KEYWORD_AND
     ) {
         Token op = consume();
         ParseResult right = parse_bitwise_xor();
-        if (right.parsed_hunk) {
-            result.parsed_hunk = reinterpret_cast<TreeBase*>(
-                new Logical{result.parsed_hunk, op, right.parsed_hunk}
+        if (right.is_usable()) {
+            result = ParseResult::Ok(
+                reinterpret_cast<TreeBase*>(
+                    new Logical{result.unwrap(), op, right.unwrap()}
+                )
             );
-        } else if (!right.error.empty()) {
-            result.parsed_hunk = nullptr;
-            result.error = right.error;
-        } else {
-            result.parsed_hunk = nullptr;
-            result.error = "Expected expression after ";
-            result.error.append(op.value);
+        } else if (result.is_null_tree()) {
+            result = ParseResult::Error(
+                "Expected expression after " + op.value
+            );
         }
     }
     return result;
-}
+} 
 
 ParseResult Parser::parse_bitwise_xor() {
     ParseResult result = parse_bitwise_or();
