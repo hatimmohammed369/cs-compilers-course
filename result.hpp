c0707c310b48bc8bddd3db2ff7e9e749fdb348b4
Hatim Muhammed <hatimmohammed369@gmail.com>
2025-02-05 16:27:45 +0200

    Code rewrite for `Result` type

--- result.hpp
+++ result.hpp
@@ -5,29 +5,32 @@
 
 template <typename T, typename E>
 class Result {
+public:
+    using ValueType = T;
+    using ErrorType = E;
 protected:
-    T value;
-    E error;
+    ValueType value;
+    ErrorType error;
     bool _is_ok;
 public:
-    static Result<T, E> Ok(T value) {
+    static Result<ValueType, ErrorType> Ok(ValueType value) {
         return Result{value, {}, true};
     }
 
-    static Result<T, E> Error(E error) {
+    static Result<ValueType, ErrorType> Error(ErrorType error) {
         return Result{{}, error, false};
     }
 
     inline bool is_ok() const noexcept { return _is_ok; }
     inline bool is_error() const noexcept { return !_is_ok; }
 
-    T unwrap() const {
+    ValueType unwrap() const {
         if (!_is_ok)
             throw std::runtime_error("Attemping to unwrap a value from an error result");
         return value;
     }
 
-    E unwrap_error() const {
+    ErrorType unwrap_error() const {
         if (_is_ok)
             throw std::runtime_error("Attemping to unwrap an error from an value result");
         return error;
