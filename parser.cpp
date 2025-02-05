8347f45ca9c13319ea69b888d343a328b63740e7
Hatim Muhammed <hatimmohammed369@gmail.com>
2025-02-05 17:28:37 +0200

    Fixing method `parse_bitwise_and`

--- parser.cpp
+++ parser.cpp
@@ -274,25 +274,22 @@ ParseResult Parser::parse_bitwise_or() {
 
 ParseResult Parser::parse_bitwise_and() {
     ParseResult result = parse_equality();
-    if (!result.error.empty() || !result.parsed_hunk)
-        return result;
     while (
-        result.error.empty() &&
+        result.is_ok() &&
         current.ttype == TokenType::BITWISE_AND
     ) {
         Token op = consume();
         ParseResult right = parse_equality();
-        if (right.parsed_hunk) {
-            result.parsed_hunk = reinterpret_cast<TreeBase*>(
-                new Bitwise{result.parsed_hunk, op, right.parsed_hunk}
+        if (right.is_usable()) {
+            result = ParseResult::Ok(
+                reinterpret_cast<TreeBase*>(
+                    new Bitwise{result.unwrap(), op, right.unwrap()}
+                )
             );
-        } else if (!right.error.empty()) {
-            result.parsed_hunk = nullptr;
-            result.error = right.error;
-        } else {
-            result.parsed_hunk = nullptr;
-            result.error = "Expected expression after ";
-            result.error.append(op.value);
+        } else if (result.is_null_tree()) {
+            result = ParseResult::Error(
+                "Expected expression after " + op.value
+            );
         }
     }
     return result;
